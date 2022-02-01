bdec9d7a92c085ac52f5be33e2171e50384b74d4
Ivan Petr <ivan.kiel@gmail.com>
2022-02-01 14:50:53 +0800

    listview added

--- app/src/main/java/com/example/recyclerviewcolorsk/ColorAdapter.kt
+++ app/src/main/java/com/example/recyclerviewcolorsk/ColorAdapter.kt
@@ -1,8 +1,21 @@
 package com.example.recyclerviewcolorsk
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 
+/*
 class ColorAdapter(private val inflater: LayoutInflater) :
     ListAdapter<Int, ColorViewHolder>(ColorDiffer) {
-
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ColorViewHolder {
+        TODO("Not yet implemented")
     }
+
+
+
+    override fun onBindViewHolder(holder: ColorViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
+*/
--- app/src/main/java/com/example/recyclerviewcolorsk/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewcolorsk/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.recyclerviewcolorsk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
+
+    // TODO: реализовать генерацию случайного списка строк
+    // и кнопку для перегенрации и отображения нового списка
+    val list = arrayListOf<String>("Mars", "Venus", "Earth")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val lv = findViewById<ListView>(R.id.list)
+        val adapter = ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, list)
+        lv.adapter = adapter
     }
 }
